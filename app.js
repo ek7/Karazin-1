62432cfa66895798d01d3267053a32872c349794
ek7 <kentikzaraki@gmail.com>
2015-10-18 12:15:15 +0300

    new commit

--- app.js
+++ app.js
@@ -21,11 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/table',routes);
-app.use('/json',routes);
-app.use('/:par',routes);
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
